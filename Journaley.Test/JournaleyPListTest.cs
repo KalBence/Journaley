87521eb6e613c194cc417e028d43b5b94679fda0
DDoSinho <daniel.kovacs314@gmail.com>
2017-05-02 16:17:26 +0200

    add journaleyplisttest initplisttest

---
+++ Journaley.Test/JournaleyPListTest.cs
@@ -0,0 +1,26 @@
+﻿using Journaley.Core.PList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Journaley.Test
+{
+    [TestClass()]
+    public class JournaleyPListTest
+    {
+        [TestMethod()]
+        public void InitPListReal()
+        {
+            decimal value = 5;
+
+            PListReal pListReal = new PListReal(value);
+
+            Assert.AreEqual(pListReal.Value, value);
+
+        }
+    }
+}
