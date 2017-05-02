a828027c0003340af086e956d87975c4531a4515
KalBence <kalman.bence95@gmail.com>
2017-05-02 20:25:47 +0200

    FirstSentenceExtractor class UnitTest

--- Journaley.Test/ExtractFirsLineTest.cs
+++ Journaley.Test/ExtractFirsLineTest.cs
@@ -19,7 +19,7 @@ namespace Journaley.Test
         {
             string empty = null;
 
-            Assert.AreEqual("", FirstSentenceExtractor.ExtractFirstLine(empty));
+            Assert.AreEqual(string.Empty, FirstSentenceExtractor.ExtractFirstLine(empty));
         }
 
         /// <summary>
@@ -36,7 +36,82 @@ namespace Journaley.Test
             Assert.AreEqual("Lorem ipsum dolor sit amet", FirstSentenceExtractor.ExtractFirstLine(sb.ToString()));
         }
 
+        /// <summary>
+        /// Tests if the function returns with null - empty string tuple
+        /// </summary>
+        [TestMethod()]
+        public void TitleAndFirstSentenceEmptyTest()
+        {
+            var actual = FirstSentenceExtractor.ExtractTitleAndFirstSentence(string.Empty);
+            var expected = new Tuple<string, string>(null, string.Empty);
 
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Tests if the function returns with null - first sentence, when the first line has punctuation mark
+        /// </summary>
+        [TestMethod()]
+        public void TitleAndFirstSentenceWithPunctuationTest()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("This is a sentence with a punctuation mark.");
+            sb.Append(Environment.NewLine);
+            sb.Append("Lorem ipsum dolor sit amet");
+
+            var actual = FirstSentenceExtractor.ExtractTitleAndFirstSentence(sb.ToString());
+            var expected = new Tuple<string, string>(null, "This is a sentence with a punctuation mark.");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Tests if the function returns with first line - first sentence of the second line,
+        /// when the first line doesn't have punctuation mark but the second one does.
+        /// </summary>
+        [TestMethod()]
+        public void TitleAndFirstSentenceWithoutPunctuationTest1()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("This is a sentence without a punctuation mark");
+            sb.Append(Environment.NewLine);
+            sb.Append("Lorem ipsum dolor sit amet! consectetur adipiscing elit");
+
+            var actual = FirstSentenceExtractor.ExtractTitleAndFirstSentence(sb.ToString());
+            var expected = new Tuple<string, string>("This is a sentence without a punctuation mark", "Lorem ipsum dolor sit amet!");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Tests if the function returns with first line -  second line,
+        /// when the first and the second line doesn't have punctuation mark 
+        /// </summary>
+        [TestMethod()]
+        public void TitleAndFirstSentenceWithoutPunctuationTest2()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("This is a sentence without a punctuation mark");
+            sb.Append(Environment.NewLine);
+            sb.Append("Lorem ipsum dolor sit amet");
+
+            var actual = FirstSentenceExtractor.ExtractTitleAndFirstSentence(sb.ToString());
+            var expected = new Tuple<string, string>("This is a sentence without a punctuation mark", "Lorem ipsum dolor sit amet");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Tests if the function returns with the one line - empty string, if that one line is the only input
+        /// </summary>
+        [TestMethod()]
+        public void TitleAndFirstSentenceOneLongTest()
+        {
+            var actual = FirstSentenceExtractor.ExtractTitleAndFirstSentence("This line is the only input");
+            var expected = new Tuple<string, string>("This line is the only input", string.Empty);
+
+            Assert.AreEqual(expected, actual);
+        }
 
     }
 }
