57fb7505c3e2fd71d5e0a6e0eb7e7872e59fb2c5
DDoSinho <daniel.kovacs314@gmail.com>
2017-05-02 16:38:16 +0200

    add journaleyplisttest plistrealequals

--- Journaley.Test/JournaleyPListTest.cs
+++ Journaley.Test/JournaleyPListTest.cs
@@ -13,14 +13,25 @@ namespace Journaley.Test
     public class JournaleyPListTest
     {
         [TestMethod()]
-        public void InitPListReal()
+        public void PListRealConstructor()
         {
             decimal value = 5;
 
             PListReal pListReal = new PListReal(value);
 
             Assert.AreEqual(pListReal.Value, value);
-
         }
+
+        [TestMethod()]
+        public void PListRealEquals()
+        {
+            decimal constructorParameter = 10;
+
+            PListReal plistReal1 = new PListReal(constructorParameter);
+            PListReal plistReal2 = new PListReal(constructorParameter);
+
+            Assert.IsTrue(plistReal1.Equals(plistReal2));
+        }
+
     }
 }
